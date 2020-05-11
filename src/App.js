1e2eeee8b34e83558a9e6dec5460d79db064a94b
Rohit Bansal <rohit.bansal@xebia.com>
2020-05-11 19:10:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import allReducers from './reducers';
 import AppNavigation from './navigation/AppNavigation';
 import creatSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/rootSaga';
-import {AsyncStorage} from 'react-native';
-import PushNotification from 'react-native-push-notification';
 
 const sagaMiddleware = creatSagaMiddleware();
 let store = createStore(allReducers, applyMiddleware(sagaMiddleware));
